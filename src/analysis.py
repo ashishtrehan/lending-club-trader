4ffe83c8958765d42a11e17d106a6d60998d680c
Ashish Trehan <ashishtrehan10@gmail.com>
2018-12-28 13:48:48 -0500

    building metrics in class LCTrader

--- src/analysis.py
+++ src/analysis.py
@@ -1,13 +1,21 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from connection import engine_db
 
 
 
 class LCTrader(object):
     def __init__(self):
-        None
         self.loan_data = pd.read_sql_query('select * from lending_club.loans',con=engine_db)
 
+        #top employ
+        self.employment = self.loan_data['emp_title'].value_counts().head()
+
+        self.employment_length = self.loan_data['emp_length'].value_counts().head()
 
 
-print (LCTrader().loan_data)
+
+
+
+print (LCTrader().employment_length)
